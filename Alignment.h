2bf800903edebd58891376e10c6450f513b9cd8b
yao.ornl <yao.ornl@gmail.com>
2015-02-11 22:58:36 +0000

    test on the query data input module

---
+++ Alignment.h
@@ -0,0 +1,129 @@
+/*
+ * Alignment.h
+ *
+ *  Created on: Dec 23, 2014
+ *      Author: qy2
+ */
+
+#ifndef ALIGNMENT_H_
+#define ALIGNMENT_H_
+
+#include "Config.h"
+#include "QueryRead.h"
+#include "SubjectRead.h"
+
+class QueryRead;
+
+
+
+
+class Alignment {
+
+
+
+public:
+	Alignment();
+	Alignment(SubjectRead* sRead);
+	Alignment(QueryRead* qRead);
+	Alignment(SubjectRead* sRead, QueryRead* qRead);
+	~Alignment();
+
+	/*
+	// Don't need these two variables for error correction to save memory.
+	string subjectReadSequence;
+	string subjectReadName;
+	*/
+	SubjectRead * subjectRead;
+	//alignment is subject to the subject reads, which is on top of the orientation graph
+
+	QueryRead * queryRead;
+
+	// orientation of the query read
+	bool queryOrientation;		// true for forward, false for reverse
+
+//******OMEGA original definition for the alignment orientation******
+	// orient 0
+	//   >--------MMMMMMMMMMMMMMM*************> 			read1      M means match found by hash table
+	//            MMMMMMMMMMMMMMM*************------->      read2      * means we need to check these characters for match
+
+	// orient 2
+	//	 >---*****MMMMMMMMMMMMMMM*************> 			read1
+	//		      MMMMMMMMMMMMMMM*************-------<	    Reverese complement of read2
+
+	// orient 1
+	//   	>********MMMMMMMMMMMMMMM-------------> 			read1      M means match found by hash table
+	//  >----********MMMMMMMMMMMMMMM       		    		read2      * means we need to check these characters for match
+
+	// orient 3
+	//	 	>********MMMMMMMMMMMMMMM-------------> 			read1
+	//	<----********MMMMMMMMMMMMMMM						Reverse Complement of Read2
+
+//*******************************************************************
+
+
+	// coordinates of the overlap alignment, which is defined by the query reads
+	//  The following case will have a positive subject start position
+	//  query:        XXXXXXMMMMMMMM
+	//	subject:            MMMMMMMMXXXXXXX
+	//  The following case will have a negative subject start position
+	//  query:        MMMMMMXXXXXXXX
+	//	subject: XXXXXMMMMMM
+	int subjectStart;
+	int queryEnd;
+	int subjectEnd;
+
+	UINT8 containedTag; //0-not contained alignment, 1-subject is contained in query 2-query is contained in subject
+
+
+
+	int orientationTranslate();
+	bool isContainedAlignment();
+	int getEditDistance(); //return the size of the editInfor
+
+private:
+	// coordinates are defined by the query reads
+	// all insertion and deletion is on the subject read
+	// upper case char means substitution
+	// lower case char means insertion
+	// 'D' means deletion
+	//
+	map<int, char>* editInfor;
+};
+
+/*
+//class edge is used by OverlapGraphConstructor
+class edge{
+public:
+	// alignment input
+	string subjectReadSequence;
+	string subjectReadName;
+
+
+
+	// alignment results
+	vector<Alignment*> alignmentList;
+
+	// a list of query reads that are contained by the subject read or duplicate with the subject read
+	// when a query is duplicate with a subject read, we compare their names and add the query read to the duplicate read list if its name is greater than the subject read name.
+	vector<QueryRead *> containedOrDeplicateReadList;//It's all for contained read if the duplicated reads are removed from the beginning.
+	edge();
+	~edge();
+
+};
+
+
+//class SubjectAlignment is used by ErrorCorrector
+class SubjectAlignment {
+public:
+	// alignment input
+	string subjectReadSequence;
+	string subjectReadName;
+
+	// alignment results
+	vector<Alignment*> queryAlignmentList;
+	SubjectAlignment();
+	~SubjectAlignment();
+
+};
+*/
+#endif /* ALIGNMENT_H_ */
---
+++ QueryRead.h
@@ -0,0 +1,45 @@
+/*
+ * QueryRead.h
+ *
+ *  Created on: Dec 23, 2014
+ *      Author: qy2
+ */
+
+#ifndef QUERYREAD_H_
+#define QUERYREAD_H_
+
+#include "Config.h"
+#include "Alignment.h"
+class Alignment;
+class QueryRead {
+	vector<Alignment*>* queryAlignmentList;
+	string readSequence;
+	string readName;
+	UINT64 readID; 		// Unique Identification of the read. start from one. zero means a new read.This is used in hashtable.
+	UINT32 frequency; 	// Frequency of the read. Number of times this read is present in the dataset. Used in some statistical analysis.
+	string correctedRead;
+
+
+public:
+	bool flag4Removal; // this is a contained or duplicate read
+	QueryRead();
+	QueryRead(string & sequence, string & name);
+	~QueryRead();
+	bool addAlignment(Alignment* subjectAlignment);
+	bool correctErrors();
+	bool needRemoval();
+	void setName(string & name);
+	void setSequence(string & sequence);
+	void setFrequency(UINT32 number);
+	void setIdentifier(UINT64 id);
+	string getName();
+	string getSequence();
+	UINT64 getIdentifier();
+	UINT32 getFrequency();
+	UINT32 getReadLength();
+	static string reverseComplement(const string & read);
+	string reverseComplement();
+	bool printAlignmentToFile(ofstream & filePointer);
+};
+
+#endif /* QUERYREAD_H_ */
