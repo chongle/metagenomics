2427ca827ec74254f272470eb6da7f15ebdb001e
yao.ornl@gmail.com <yao.ornl@gmail.com@016e7d22-b25e-d8ca-1779-6aa53390b87f>
2015-02-11 22:20:34 +0000

    test on data input module

---
+++ omega_test/Read.h
@@ -0,0 +1,74 @@
+/*
+ * Reads.h
+ *
+ * Created on: April 22, 2013
+ * Author: Md. Bahlul Haider
+ */
+
+
+#ifndef READ_H_
+#define READ_H_
+
+#include "Common.h"
+
+class Edge;
+
+struct MPlist
+{
+	UINT64 matePairID;				// This list stores the list of ID's of the matepairs of the current read.
+	UINT8 matePairOrientation; 		// List of mate pair orientation.
+									// 0 = 00 means the reverse of this read and the reverse of the second read are matepairs.
+									// 1 = 01 means the reverse of this read and the forward of the second read are matepairs.
+									// 2 = 10 means the forward of this read and the reverse of the second read are matepairs.
+									// 3 = 11 means the forward of this read and the forward of the second read are matepairs.
+	UINT8 datasetNumber;			//Dataset number.
+};
+
+/**********************************************************************************************************************
+	Class to store a read.
+**********************************************************************************************************************/
+
+class Read
+{
+	private:
+		UINT64 readNumber; 						// Unique Identification of the read.
+		string read; 							// String representation of the read.
+		string readReverse;
+		UINT32 frequency; 						// Frequency of the read. Number of times this read is present in the dataset. Used in some statistical analysis.
+		vector<MPlist> *matePairList;
+		vector<Edge *> *listOfEdgesForward;   	// List of edges that contain the forward string of this read.
+		vector<UINT64> *locationOnEdgeForward;	// List of locations on the edges that contain the forward string of the current read.
+		vector<Edge *> *listOfEdgesReverse;		// List of edges that contain the reverse string of this read.
+		vector<UINT64> *locationOnEdgeReverse;	// List of locations on the edges that contain the reverse string of the current read.
+
+		string reverseComplement(const string & read);
+
+	public:
+		UINT64 coverageDepth;					// Estimated depth of coverage.  --> Not use anymore
+		UINT64 locationInDataset;				// Not for debugging only. Will be removed.  --> Not use anymore
+		bool isContainedRead;
+		UINT64 superReadID;						// 0 = not a contained read
+												// otherwise superReadID contains the ID of the uniqe super read.
+		Read(void);								// Default constructor.
+		Read(const string & s);					// Another constructor.
+		~Read(void);							// Destructor.
+
+		bool setRead(const string & s); 		// Set the read.
+		bool setReadNumber(UINT64 id); 			// Set the read number.
+		bool setFrequency(UINT32 freq);			// Set the ferquency of the read.
+
+		string getStringForward(void){return read;} 									// Get the forward string of the current read.
+		string getStringReverse(void){return readReverse;} 								// Get the reverse string of the current read.
+		UINT16 getReadLength(void){return read.length();} 								// Get the length of the string in the current read.
+		UINT64 getReadNumber(void) {return readNumber;} 								// Get the read number of the current read.
+		UINT32 getFrequency(void) {return frequency;}									// Get the frequency of the current read.
+		vector<MPlist> * getMatePairList(void) {return matePairList;} 					// Get the list of matepairs.
+		vector<Edge *> * getListOfEdgesForward(void){return listOfEdgesForward;}		// Get the list of edges that contain the forward string of the current read.
+		vector<UINT64> * getLocationOnEdgeForward(void){return locationOnEdgeForward;}	// Get the list of locations on the edges that contain the forward string of the current read.
+		vector<Edge *> * getListOfEdgesReverse(void){return listOfEdgesReverse;}		// Get the list of edges that contain the reverse string of the current read.
+		vector<UINT64> * getLocationOnEdgeReverse(void){return locationOnEdgeReverse;}	// Get the list of locations on the edges that contain the reverse string of the current read.
+		bool addMatePair(Read *r, UINT8 orientation, UINT64 datasetNumber);				// Add a matepair in the list.
+
+};
+
+#endif /* READS_H_ */
---
+++ omega_test/main.cpp
@@ -0,0 +1,231 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 11, 2015
+ *      Author: qy2
+ */
+/*
+ * main.cpp
+ *
+ * Created on: April 22, 2013
+ * Author: Md. Bahlul Haider
+ * Version: 1.0.1 (Beta) - Tae-Hyuk (Ted) Ahn: Validate input arguments
+ */
+
+
+#include "Common.h"
+#include "Read.h"
+#include "Dataset.h"
+//YAO #include "HashTable.h"
+//YAO #include "Edge.h"
+//YAO #include "OverlapGraph.h"
+
+
+//=============================================================================
+// Help usage
+//=============================================================================
+void usage();
+
+
+//=============================================================================
+// Help usage
+//=============================================================================
+void usage()
+{
+    std::cout << std::endl
+              << "  Usage:" << std::endl
+              << "    omega [OPTION]...<PARAM>..." << std::endl
+              << std::endl
+              << "  <PARAM>" << std::endl
+              << "    -pe\tpaired-end file names (comma separated)" << std::endl  // Paired-end file name in fasta/fastq format. mate pairs should be one after another in the file.
+              << "    -se\tsingle-end file names (comma separated)" << std::endl  // Single-end file name in fasta/fastq format.
+              << "    -l\tminimum overlap length" << std::endl  // Minimum overlap length for two reads to overlap in the overlap graph.
+              << std::endl
+              << "  [OPTION]" << std::endl
+              << "    -h/--help" << std::endl
+              << "    -f\tAll file name prefix (default: output)" << std::endl    // all output file with have this name with different extensions.
+              << "    -s\tstart from unitig graph" << std::endl // -s means that the program will build the graph. Otherwise it will load the graph from the unitig graph file.
+              << std::endl;
+}
+
+
+void parseArguments(int argc, char **argv, vector<string> & pairedEndFileNames, vector<string> & singleEndFileNames,string & allFileName, UINT64 & minimumOverlapLength, bool & startFromUnitigGraph);
+
+int main(int argc, char **argv)
+{
+	CLOCKSTART;
+	MEMORYSTART;
+	UINT64 minimumOverlapLength;
+	vector<string> pairedEndFileNames, singleEndFileNames;
+	UINT64 counter;
+	UINT64 iteration = 0;
+	string allFileName;
+	bool startFromUnitigGraph = false;
+	parseArguments(argc, argv, pairedEndFileNames, singleEndFileNames, allFileName, minimumOverlapLength, startFromUnitigGraph);
+	Dataset *dataSet = new Dataset(pairedEndFileNames, singleEndFileNames, minimumOverlapLength);
+	MEMORYSTOP;
+	CLOCKSTOP;
+	//YAO 	OverlapGraph *overlapGraph;
+
+	//YAO 	if(startFromUnitigGraph) // Read the graph from unitig file
+	//YAO 	{
+	//YAO 		overlapGraph = new OverlapGraph();
+	//YAO 		overlapGraph->setDataset(dataSet);
+	//YAO 		overlapGraph->readGraphFromFile(allFileName+"_unitig.unitig");
+	//YAO 		overlapGraph->sortEdges();
+	//YAO 	}
+	//YAO 	else // Build the graph from the scratch
+	{
+		//YAO		HashTable *hashTable=new HashTable();
+		//YAO		hashTable->insertDataset(dataSet, minimumOverlapLength);
+		//YAO 		overlapGraph=new OverlapGraph(hashTable); //hashTable deleted by this function after building the graph
+		//YAO 		dataSet->saveReads(allFileName+"_sortedReads.fasta");
+		//YAO 		overlapGraph->sortEdges();
+		//YAO 		overlapGraph->saveGraphToFile(allFileName+"_unitig.unitig");
+	}
+/*YAO
+	overlapGraph->calculateFlow(allFileName+"_flow.input", allFileName+"_flow.output");
+	cout << "nodes: " << overlapGraph->getNumberOfNodes() << " edges: " << overlapGraph->getNumberOfEdges() << endl;
+	overlapGraph->printGraph(allFileName+"_graph1.gdl", allFileName+"_contigs1.fasta");
+
+	overlapGraph->removeAllSimpleEdgesWithoutFlow();
+
+	overlapGraph->calculateMeanAndSdOfInsertSize();
+
+
+
+	do
+	{
+		// Mate pair paths are used to simplify the graph in this step
+		cout << endl;
+		cout << "===============================================================================================================================================" <<endl;
+		cout << "FIRST LOOP ITERATION " << ++iteration << endl;
+		cout << "===============================================================================================================================================" <<endl;
+		overlapGraph->simplifyGraph();
+		counter = overlapGraph->findSupportByMatepairsAndMerge();
+	} while (counter > 0 && iteration < loopLimit); // To avoid infinite loops
+
+	overlapGraph->printGraph(allFileName+"_graph2.gdl", allFileName+"_contigs2.fasta");
+
+	iteration = 0;
+	do
+	{
+		// Scaffolder
+		cout << endl;
+		cout << "===============================================================================================================================================" <<endl;
+		cout << "SECOND LOOP ITERATION " << ++iteration << endl;
+		cout << "===============================================================================================================================================" <<endl;
+		overlapGraph->simplifyGraph();
+		counter = overlapGraph->scaffolder();
+
+	} while (counter > 0 && iteration < loopLimit);// To avoid infinite loops
+
+	overlapGraph->printGraph(allFileName+"_graph3.gdl", allFileName+"_contigs3.fasta");
+
+	iteration = 0;
+	do
+	{
+		// Coverage depth information is used to resolve ambiguity
+		cout << endl;
+		cout << "===============================================================================================================================================" <<endl;
+		cout << "THIRD LOOP ITERATION " << ++iteration << endl;
+		cout << "===============================================================================================================================================" <<endl;
+		overlapGraph->simplifyGraph();
+		counter = overlapGraph->resolveNodes();
+
+	} while (counter > 0 && iteration < loopLimit);// To avoid infinite loops
+
+	overlapGraph->printGraph(allFileName+"_graph4.gdl", allFileName+"_contigs4.fasta");
+*/
+	delete dataSet;
+//YAO	delete overlapGraph;
+
+}
+
+
+
+/**********************************************************************************************************************
+	Parse the input arguments
+**********************************************************************************************************************/
+
+void parseArguments(int argc, char **argv, vector<string> & pairedEndFileNames, vector<string> & singleEndFileNames, string & allFileName, UINT64 & minimumOverlapLength, bool & startFromUnitigGraph)
+{
+	allFileName = "output";
+	minimumOverlapLength = 0;
+	startFromUnitigGraph = false;
+	vector<string> argumentsList;
+	cout << "PRINTING ARGUMENTS" << endl;
+	for(UINT64 i = 0; i < argc; i++)
+	{
+		cout << argv[i] << ' ';
+	}
+	cout << endl;
+	while(argc--)
+			argumentsList.push_back(*argv++);
+
+	if(argumentsList.size() == 1)
+	{
+        usage();
+		exit(0);
+	}
+
+	for(UINT64 i = 1; i <= argumentsList.size()-1; i++)
+	{
+		if (argumentsList[i] == "-h" || argumentsList[i] == "--help")
+	    {
+            usage();
+		    exit(0);
+	    }
+		else if(argumentsList[i] == "-pe")
+		{
+			string inputFilenames=argumentsList[++i];
+            stringstream ss(inputFilenames);
+            string item;
+
+			while (getline(ss, item, ','))
+			{
+				pairedEndFileNames.push_back(item);
+			}
+		}
+		else if(argumentsList[i] == "-se")
+		{
+			string inputFilenames=argumentsList[++i];
+            stringstream ss(inputFilenames);
+            string item;
+
+			while (getline(ss, item, ','))
+			{
+				singleEndFileNames.push_back(item);
+			}
+		}
+		else if (argumentsList[i] == "-f")
+			allFileName = argumentsList[++i];
+		else if (argumentsList[i] == "-l")
+			minimumOverlapLength = atoi(argumentsList[++i].c_str());
+		else if (argumentsList[i] == "-s")
+			startFromUnitigGraph = true;
+		else
+		{
+            usage();
+		    exit(0);
+		}
+	}
+
+    if(minimumOverlapLength == 0)
+    {
+        cout << "missed -l option!" << std::endl;
+        usage();
+		exit(0);
+    }
+
+    if( (pairedEndFileNames.size() == 0) && (singleEndFileNames.size() == 0) )
+    {
+        cout << "missed -pe or -se input files!" << std::endl;
+        usage();
+		exit(0);
+    }
+}
+
+
+
+
